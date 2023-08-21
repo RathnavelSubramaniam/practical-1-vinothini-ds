adeba27d57502376871885ca26de3323b5d50c63
vinothini-ds <142780008+vinothini-ds@users.noreply.github.com>
2023-08-21 08:52:47 +0000

    completed area of the circle

---
+++ area.java
@@ -0,0 +1,62 @@
+class area {
+    public static void main(String args[]){
+        double a,pi=3.14,r=2.4;
+        a=pi*r*r;
+        System.out.println("area of the circle is="+a);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
